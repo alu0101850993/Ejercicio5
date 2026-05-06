76ada702441307f3c37f6029f9dab228a5c9147f
sergiioomr <alu0101718194@ull.edu.es>
2026-05-06 10:35:00 +0100

    Clase mensaje para el fichero notificaciones

---
+++ notificaciones.h
@@ -0,0 +1,19 @@
+// Función para mostrar una notificación, esta recibe un mensaje en forma de string y muestra lo que se encuentre en el mimso
+#include <iostream>
+#include <string>
+
+class Mensaje {
+  public:
+    Mensaje(const std::string &mensaje, const std::string &fecha) : contenido_(mensaje), fecha_(fecha) {}
+    
+    std::string GetFecha() const { return fecha_; }
+    std::string GetContenido() const { return contenido_; }
+
+  private:
+    std::string contenido_;
+    std::string fecha_;
+};
+
+std::ostream& operator<<(std::ostream& os, const Mensaje &mensaje) {
+  os << "Notificación entrante [" << mensaje.GetFecha() << "] : " << mensaje.GetContenido() << std::endl;
+}   
